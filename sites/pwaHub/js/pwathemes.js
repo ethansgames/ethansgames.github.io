378468e896cb07e1a65e55d9c681ab2f67ac7dc8
GrepBDS <277907605+grepBDS@users.noreply.github.com>
2026-06-27 22:53:24 -0600

    Fixed Siege Protecters archive broken links and other experimentals bugs, added a pwa hub that has themes

---
+++ sites/pwaHub/js/pwathemes.js
@@ -0,0 +1,133 @@
+const header=document.getElementById("hdr");
+const links=document.getElementById("links");
+const herotitle=document.getElementById("ht");
+const footer=document.getElementById("footer");
+const gtspan=document.getElementById("titleGameSpan");
+const logo=document.getElementById("lg");
+const linkbuttons = document.querySelectorAll('.link-button');
+const experimentalbutton = document.getElementById("xplb");
+const body = document.querySelectorAll("body");
+const ftlicense = document.getElementById("ftgamelicense");
+const ftdisclaimer = document.getElementById("ftdisclaimer");
+const nextThemeButton = document.getElementById("nextThemeButton");
+
+let themeIndex = 0;
+
+nextThemeButton.addEventListener('click', _ => {
+    if (themeIndex != themes.length - 1) {
+        themeIndex++;
+    } else {themeIndex = 0;}
+    
+    updatetheme(themeIndex);
+
+    nextThemeButton.textContent = themes[themeIndex].name;
+    nextThemeButton.style.background = themes[themeIndex].lbGradient;
+    nextThemeButton.style.color = themes[themeIndex].logocolor;
+    nextThemeButton.style.border = "4px solid " + themes[themeIndex].border;
+})
+
+const themes = [
+    {
+        border: "#9dff00",
+        fill: "#003b63",
+        headerlnkbg:"rgba(0, 85, 138, 1)",
+        httextShadow:"5px 5px #001b33, 0 0 15px #00eaff",
+        gtspancolor:"yellow",
+        logotextShadow:"3px 3px #18005c",
+        lbGradient:"linear-gradient( #006eff, #00ff62)",
+        lbBorder:"4px solid #faf500",
+        lbTextColor:"#00304f",
+        exbBorder:"4px solid #2b2b2b",
+        bgGradient:"linear-gradient(rgba(0, 20, 40, 0.75), rgba(0, 20, 40, 0.9)), repeating-linear-gradient( 45deg, #072b4f 0px, #072b4f 40px, #0a3766 40px, #0a3766 80px )",
+        htcolor:"#fff",
+        logocolor:"#fff",
+        footercolor:"#fff",
+        ftlinkcolor:"rgb(173, 252, 255)",
+        name:"Geometry Theme"
+    },
+    {
+        border:"red",
+        fill:"rgba(16, 16, 16, 0.82)",
+        headerlnkbg:"rgb(28, 28, 28)",
+        httextShadow:"5px 5px #000000, 0 0 15px #ffb8b8",
+        gtspancolor:"red",
+        logotextShadow:"3px 3px #000000",
+        lbGradient:"linear-gradient(rgb(49, 49, 49), rgba(33, 33, 33, 0.82))",
+        lbBorder:"4px solid red",
+        lbTextColor:"white",
+        exbBorder:"4px solid #750000",
+        bgGradient:"linear-gradient(rgba(33, 33, 33, 0.75), rgba(33, 33, 33, 0.9)), repeating-linear-gradient( 45deg, #202020 0px, #202020 40px, #151515 40px, #151515 80px )",
+        htcolor:"#fff",
+        logocolor:"#fff",
+        footercolor:"#ffffff",
+        ftlinkcolor:"rgb(255, 0, 0)",
+        name:"Black/Red Theme"
+    },
+    {
+        border:"rgb(13, 13, 13)",
+        fill:"rgba(16, 16, 16, 0.82)",
+        headerlnkbg:"rgb(28, 28, 28)",
+        httextShadow:"5px 5px #000000, 0 0 15px #454545",
+        gtspancolor:"rgb(255, 255, 255)",
+        logotextShadow:"3px 3px #000000",
+        lbGradient:"linear-gradient(rgb(49, 49, 49), rgba(33, 33, 33, 0.82))",
+        lbBorder:"4px solid rgb(13, 13, 13)",
+        lbTextColor:"white",
+        exbBorder:"4px solid #000000",
+        bgGradient:"linear-gradient(rgba(33, 33, 33, 0.75), rgba(33, 33, 33, 0.9)), repeating-linear-gradient( 45deg, #505050 0px, #505050 40px, #373737 40px, #373737 80px )",
+        htcolor:"#fff",
+        logocolor:"#fff",
+        footercolor:"#fff",
+        ftlinkcolor:"rgb(173, 252, 255)",
+        name:"Dark Mode"
+    },
+    {
+        border:"rgb(178, 178, 178)",
+        fill:"rgba(234, 234, 234, 0.82)",
+        headerlnkbg:"rgb(255, 255, 255)",
+        httextShadow:"5px 5px #c8c8c8, 0 0 15px #ffffff",
+        gtspancolor:"rgb(255, 242, 0)",
+        logotextShadow:"3px 3px #c8c8c8",
+        lbGradient:"linear-gradient(rgb(240, 240, 240), rgba(226, 226, 226, 0.82))",
+        lbBorder:"4px solid rgb(212, 212, 212)",
+        lbTextColor:"#6e6e6e",
+        exbBorder:"4px solid #b9b9b9",
+        bgGradient:"linear-gradient(rgba(255, 255, 255, 0.75), rgba(255, 255, 255, 0.9)), repeating-linear-gradient( 45deg, #f6f6f6 0px, #f6f6f6 40px, #eaeaea 40px, #eaeaea 80px )",
+        htcolor:"#6e6e6e",
+        logocolor:"#6e6e6e",
+        footercolor:"#6e6e6e",
+        ftlinkcolor:"#26a8ff",
+        name:"Lite Mode"
+    }
+]
+function updatetheme(themeNum) {
+    header.style.borderBottom = "4px solid " + themes[themeNum].border;
+    footer.style.borderTop = "4px solid " + themes[themeNum].border;
+    links.style.border = "4px solid " + themes[themeNum].border;
+
+    header.style.background = themes[themeNum].headerlnkbg;
+    links.style.background = themes[themeNum].headerlnkbg;
+    footer.style.background = themes[themeNum].fill;
+    body[0].style.background = themes[themeNum].bgGradient;
+
+    linkbuttons.forEach(element => {
+        element.style.background = themes[themeNum].lbGradient;
+        element.style.border = themes[themeNum].lbBorder;
+        element.style.color = themes[themeNum].lbTextColor;
+    });
+
+    ftdisclaimer.style.color = themes[themeNum].ftlinkcolor;
+    ftlicense.style.color = themes[themeNum].ftlinkcolor;
+
+    experimentalbutton.style.border = themes[themeNum].exbBorder;
+
+    herotitle.style.color = themes[themeNum].htcolor;
+    herotitle.style.textShadow = themes[themeNum].httextShadow;
+
+    gtspan.style.color = themes[themeNum].gtspancolor;
+
+    logo.style.textShadow = themes[themeNum].logotextShadow;
+    logo.style.color = themes[themeNum].logocolor;
+
+    footer.style.color = themes[themeNum].footercolor;
+}
